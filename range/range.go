48329593552fef1b5c0230a71a7092c55f2c8c65
dfchan <dfchan@idsdatanet.com>
2022-01-21 09:43:00 +0800

    range lesson

---
+++ range/range.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2, 3, 4}
+	sum := 0
+	//the _ is when we don't need the index, implicitly, index is supplied
+	//we have to supply it or it will treat the num:=range nums as a condition
+	for _, num := range nums {
+		sum += num
+	}
+
+	fmt.Println("sum: ", sum)
+
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index: ", i)
+		}
+	}
+
+	//it can iterate over maps
+	kvs := map[string]int{"a": 1, "b": 2}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %d\n", k, v)
+	}
+
+	//iterate over keys of the map
+	for k := range kvs {
+		fmt.Println("key: ", k)
+	}
+
+	for i, c := range "hello world" {
+		fmt.Println(i, c)
+	}
+}
