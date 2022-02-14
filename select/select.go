f7c4fe95dcf73449df952d0e056e732a50761db6
dfchan <dfchan@idsdatanet.com>
2022-02-14 08:52:38 +0800

    select

---
+++ select/select.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/*
+	essentially it is a waiting group for multiple channels. The process will end when all the channels return a value.
+*/
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "tw0"
+	}()
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received:", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received:", msg2)
+		}
+	}
+}
